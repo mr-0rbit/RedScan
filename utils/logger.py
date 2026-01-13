ca5a993d50f8c7b14b1b505ce3638ccf8b3a2b96
0x0r6it Official <penetrationtesterofficial@gmail.com>
2026-01-14 02:38:37 +0500

    Add files via upload

---
+++ utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+def setup_logger(verbose=False):
+    level = logging.DEBUG if verbose else logging.INFO
+
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    return logging.getLogger("reconix")
